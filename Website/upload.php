f8a89eccb0f39f2a14f9bb2bdc774c1478597bc8
Teddabeertje <50151141+Teddabeertje@users.noreply.github.com>
2019-05-06 02:52:00 +0200

    Made the uploaded html

--- Website/upload.php
+++ Website/upload.php
@@ -1,36 +1,29 @@
-<!-- 
-@author: Frederik Ondrikov
-@date: 29-04-2019
-@comment: This is the php file which makes it possible to upload files to the website.
--->
-
 <?php
 if (isset($_POST['submit'])) {
 	$file = $_FILES['file'];
 
-	//defining variables
-	$file_Name = $_FILES['file']['name'];
-	$file_Temp_Name = $_FILES['file']['tmp_name'];
-	$file_Size = $_FILES['file']['size']; //not used since we do not know how big the files are going to be!
-	$file_Error = $_FILES['file']['error'];
-	$file_Type = $_FILES['file']['type'];
+	$fileName = $_FILES['file']['name'];
+	$fileTmpName = $_FILES['file']['tmp_name'];
+	$fileSize = $_FILES['file']['size'];
+	$fileError = $_FILES['file']['error'];
+	$fileType = $_FILES['file']['type'];
 
-	$fileExt = explode('.', $file_Name);
+	$fileExt = explode('.', $fileName);
 	$fileActualExt = strtolower(end($fileExt));
 
-	$allowed = array('csv'); //which kind of files are allowed
+	$allowed = array('csv');
 
-	if (in_array($fileActualExt, $allowed)) {                             //Is the filetype allowed? (is it a .csv-file?)
-		if ($file_Error === 0) {										  //Are there errors with the given file?
-				$file_NameNew = uniqid('', true).".".$fileActualExt;
-				$fileDestination = 'uploads/'.$file_NameNew;
-				move_uploaded_file($file_Temp_Name, $fileDestination);
-				header("Location: indexUploadSucceeded.html");            //upload file to the map "uploads"
+	if (in_array($fileActualExt, $allowed)) {
+		if ($fileError === 0) {
+				$fileNameNew = uniqid('', true).".".$fileActualExt;
+				$fileDestination = 'uploads/'.$fileNameNew;
+				move_uploaded_file($fileTmpName, $fileDestination);
+				header("Location: uploaded.html");
 		} else {
-			echo "There was an error uploading the file!";                //There is something wrong with the given file!
+			echo "There was an error uploading the file!";
 		}
 	} else {
-		echo "You are only allowed to upload .csv files!";                //The given file is not a .csv-file!
+		echo "You can only upload .csv files!";
 	}
 }
-
+?>
